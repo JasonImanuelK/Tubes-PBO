ae8974fbbaabbf8b4ce9c59bbc42ea045e160be9
User <User@DESKTOP-NE7OUFN>
2021-11-16 18:13:43 +0700

    Tambah : Tampilan Menu Registrasi, Method Registrasi

---
+++ PropertiFix/src/view/Registrasi.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controller.MethodRegistrasi;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import model.Member;
+import model.Person;
+import model.Status;
+import model.User;
+
+/**
+ *
+ * @author User
+ */
+public class Registrasi {
+    JFrame frame;
+    JPanel panel;
+    JLabel labelJudul, labelNama, labelEmail, labelPassword, labelNomorTelepon;
+    JTextField textFieldNama, textFieldEmail, textFieldNomorTelepon;
+    JPasswordField passFieldPassword;
+    JComboBox comboNamaKategori;
+    JButton buttonLogin, buttonBack;
+    MethodRegistrasi methodRegistrasi = new MethodRegistrasi();
+    
+    public Registrasi(){
+        //Frame
+        frame = new JFrame("Registrasi");
+        frame.setSize(330, 430);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Panel
+        panel = new JPanel();
+        panel.setSize(330, 430);
+        panel.setBackground(new Color(201, 248, 201));
+        
+        //Label
+        labelJudul = new JLabel("Registrasi");
+        labelJudul.setFont(new Font("Sans-Serif", Font.BOLD, 26));
+        labelJudul.setBounds(90, 30, 200, 40);
+        labelNama = new JLabel("Nama");
+        labelNama.setFont(new Font("Sans-Serif", Font.BOLD, 14));
+        labelNama.setBounds(30, 90, 100, 40);
+        labelEmail = new JLabel("Email");
+        labelEmail.setFont(new Font("Sans-Serif", Font.BOLD, 14));
+        labelEmail.setBounds(30, 140, 100, 40);
+        labelNomorTelepon = new JLabel("No. Telepon");
+        labelNomorTelepon.setFont(new Font("Sans-Serif", Font.BOLD, 14));
+        labelNomorTelepon.setBounds(30, 190, 100, 40);
+        labelPassword = new JLabel("Password");
+        labelPassword.setFont(new Font("Sans-Serif", Font.BOLD, 14));
+        labelPassword.setBounds(30, 240, 100, 40);
+        
+        //TextField
+        textFieldNama = new JTextField();
+        textFieldNama.setBounds(130, 98, 150, 30);
+        textFieldEmail = new JTextField();
+        textFieldEmail.setBounds(130, 148, 150, 30);
+        textFieldNomorTelepon = new JTextField();
+        textFieldNomorTelepon.setBounds(130, 198, 150, 30);
+        
+        //PasswordField
+        passFieldPassword = new JPasswordField();
+        passFieldPassword.setBounds(130, 248, 150, 30);
+        
+        //Button
+        buttonLogin = new JButton("Registrasi");
+        buttonLogin.setBounds(50, 340, 100, 30);
+        buttonLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                String checkPassword = new String (passFieldPassword.getPassword());
+                if (checkPassword.length() < 8) {
+                    System.out.println(checkPassword.length());
+                    JOptionPane.showMessageDialog(null, "Password kurang dari 8 huruf!", "Registrasi", JOptionPane.INFORMATION_MESSAGE);  
+                }else{
+                    Person person = new User(textFieldNama.getText(), textFieldNomorTelepon.getText(), passFieldPassword.getText(), textFieldEmail.getText(), Status.USER, Member.BRONZE);                                                                                                   
+                    boolean tanya = methodRegistrasi.inputDataUser(person);
+                    if (tanya) {
+                        JOptionPane.showMessageDialog(null, "Akun Anda Sudah Terdaftar!", "Registrasi", JOptionPane.INFORMATION_MESSAGE);  
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Gagal Registrasi!", "Registrasi", JOptionPane.INFORMATION_MESSAGE);  
+                    }
+                }   
+//                new MainMenu();
+            }
+        });
+        
+        //Button
+        buttonBack = new JButton("Back");
+        buttonBack.setBounds(170, 340, 100, 30);
+        buttonBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Login();
+            }
+        });
+        
+        panel.setLayout(null);
+        panel.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        
+        frame.add(panel);
+        panel.add(labelJudul); 
+        panel.add(labelNama);
+        panel.add(labelEmail);
+        panel.add(labelNomorTelepon);
+        panel.add(labelPassword);
+        panel.add(textFieldNama);
+        panel.add(textFieldEmail);
+        panel.add(textFieldNomorTelepon);
+        panel.add(passFieldPassword);
+        panel.add(buttonLogin);
+        panel.add(buttonBack);
+    }
+}
